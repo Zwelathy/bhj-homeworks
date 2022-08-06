d8c05db1d504ac10a46e9713d8cbfb82da0bc862
Zwelathy <tessen666@yandex.ru>
2022-08-06 04:38:52 +0300

    feat: add game counters

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,31 @@
+let hitClick = 0;
+let missClick = 0;
+
+for (let i = 1; i < 10; i++) {
+  let clickingSpot = document.getElementById('hole' + i);
+  
+  clickingSpot.onclick = () => {
+    if (clickingSpot.className.includes('hole hole_has-mole')) {
+      hitClick += 1;
+      document.getElementById('dead').textContent = hitClick;
+        
+      if (hitClick == 10) {
+        alert('Вы победили!');
+
+        return location.reload();
+      }
+    }
+    else {
+      missClick += 1;
+      document.getElementById('lost').textContent = missClick;
+
+      if (missClick == 5) {
+        alert('Вы проиграли!');
+
+        return location.reload();
+      }
+    }
+
+  }
+
+}
