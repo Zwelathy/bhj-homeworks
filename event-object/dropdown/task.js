abf36c721d94b7cdc8431f966a97fca76d42c3a2
Zwelathy <tessen666@yandex.ru>
2022-08-17 00:44:46 +0300

    feat: add dropdown menu

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+const dropdownList = document.querySelector('.dropdown__value');
+const dropdownItem = document.querySelectorAll('.dropdown__item');
+const activeItem = document.querySelector('ul.dropdown__list');
+const listLength = dropdownItem.length;
+
+dropdownList.onclick = function() {
+  activeItem.classList.add('dropdown__list_active');
+
+  for (i = 0; i < listLength; i++) {
+    dropdownItem[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      dropdownList.textContent = event.target.textContent;
+      activeItem.classList.remove('dropdown__list_active');
+
+      return;
+      });
+  }
+
+}
