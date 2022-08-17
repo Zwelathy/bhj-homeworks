10d6e3778f75eb6f61a4cd54f504af70b067a7f2
Zwelathy <tessen666@yandex.ru>
2022-08-17 15:53:31 +0300

    feat: add tabulation

---
+++ event-object/tabs/task.js
@@ -0,0 +1,27 @@
+let menuArr = [];
+const tabMenu = document.querySelectorAll("div.tab__navigation > div");
+const tabContent = document.querySelectorAll("div.tab__contents > div");
+const menuLength = tabMenu.length;
+
+for (i = 0; i < menuLength; i++) {
+  menuArr.push(tabMenu[i].innerText);
+}
+
+for (i = 0; i < menuLength; i++) {
+  tabMenu[i].addEventListener('click', function(event) {
+    let activeTab = document.querySelector('.tab_active');
+    let activeContent = document.querySelector('.tab__content_active');
+    
+    if (activeTab.innerText != event.target.innerText) {
+      activeTab.classList.remove('tab_active');
+      activeContent.classList.remove('tab__content_active');
+      event.target.classList.add('tab_active');
+
+      let arrInx = menuArr.indexOf(event.target.innerText);
+
+      tabContent[arrInx].classList.add('tab__content_active');
+
+      return;
+    }
+  });
+}
