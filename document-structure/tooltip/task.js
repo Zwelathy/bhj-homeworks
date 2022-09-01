d0744137205f8257d8b508e27733c0fec2a3bec9
Zwelathy <tessen666@yandex.ru>
2022-09-01 15:14:09 +0300

    fix: add simple resolve

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,10 +1,41 @@
-/*'use strict';
+'use strict';
 
+//простейший вариант (изменено CSS tooltip с fixed на absolute)
+document.addEventListener('click', key => {
+  key.preventDefault();
+  let target = key.target;
+  let rect = target.getBoundingClientRect();
+  console.log(rect);
+  !document.querySelector('.tooltip_active') && (document.body.innerHTML += `<div class="tooltip tooltip_active"></div>`);
+  let tooltip = document.querySelector('.tooltip_active');
+
+  if (target.title) {
+    if (tooltip.textContent == target.title) {
+      document.body.removeChild(tooltip);
+    }
+    else {
+      tooltip.textContent = target.title;
+      tooltip.style.top = `${getPosition(rect).posY}px`;
+      tooltip.style.left = `${getPosition(rect).posX}px`;
+      //tooltip.style.position = 'absolute';
+    }
+  }
+});
+
+function getPosition(rect) {
+  let posX = rect.left;
+  let posY = rect.bottom + document.body.parentElement.scrollTop; //вычисление скролла html, а не вьюпорта
+  //console.log(document.body.scrollTop, rect.top);
+
+  return {posX: posX, posY: posY};
+}
+
+//первый вариант с затирающим подсказку скроллингом и разными позициями подсказок (нужна fixed позиция tooltip)
+/*
 const tooltipHandler = document.querySelectorAll('.has-tooltip');
 const position = ['bottom', 'bottom', 'right', 'left', 'top', 'top'];
 
 [...tooltipHandler].map((element, index) => element.setAttribute('data-position', position[index]));
-
 
 document.addEventListener('click', key => {
   key.preventDefault();
@@ -26,14 +57,6 @@ document.addEventListener('click', key => {
   }
 });
 
-//не исчезающая при скроллинге подсказка только снизу
-function getPosition(rect) {
-  let posX = rect.left;
-  let posY = rect.bottom;
-
-  return {posX: posX, posY: posY}
-}
-
 function getPosition(rect, position, tooltip) {
   let posX;
   let posY;
@@ -47,55 +70,18 @@ function getPosition(rect, position, tooltip) {
     posY = rect.top;
   }
   else if (position == 'left') {
-    posX = rect.left - tooltip.getBoundingClientRect().width;
-    posY = rect.top;
+    posX = rect.left;
+    posY = rect.top + tooltip.getBoundingClientRect().height;
   }
   else {
     posX = rect.left;
-    posY = rect.bottom;
+    posY = rect.bottom - tooltip.getBoundingClientRect().height;
   }
 
-  return {posX: posX, posY: posY}
-}
+  return {posX: posX, posY: posY};
+}*/
 
-document.addEventListener('scroll', () => {
+/*document.addEventListener('scroll', () => {
   let tooltip = document.querySelector('.tooltip_active');
   tooltip && document.body.removeChild(tooltip);
 });*/
-
-//вариант номер два
-'use strict';
-
-const tooltips = Array.from(document.querySelectorAll(".has-tooltip"));
-
-tooltips.forEach(tooltip => {
-  const element = document.createElement('div');
-  element.textContent = tooltip.title;
-
-  tooltip.addEventListener("click", (key) => {
-    key.preventDefault();
-
-    element.classList.add("tooltip");
-    tooltip.insertAdjacentElement('beforeBegin', element);
-    element.style.position = "absolute";
-
-    element.style.left = `${tooltip.getBoundingClientRect().left}px`;
-    element.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
-
-    const elements = Array.from(document.querySelectorAll(".tooltip_active"));
-    const findElement = elements.find(key => key.classList.contains("tooltip_active"));
-
-    if (findElement === undefined) {
-      element.classList.add("tooltip_active");
-    }
-    else {
-      if (findElement !== element) {
-        element.classList.add("tooltip_active");
-      }
-      
-      findElement.classList.remove("tooltip_active");
-    }
-
-  })
-
-});
